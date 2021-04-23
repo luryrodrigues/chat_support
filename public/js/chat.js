44c2982507dd1ad614b35bf4b66d9f8febabe32e
Lury Rodrigues <lurylrodrigues@gmail.com>
2021-04-23 16:59:58 -0300

    creating websocket connections

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,25 @@
 document.querySelector('#start_chat').addEventListener("click", (event) =>{
   const socket = io();
-})
+
+    // Ocultar o "iniciar chat" e aparecer o chat em si
+  const chat_help = document.getElementById("chat_help");
+  chat_help.style.display = "none";
+
+  const chat_in_support = document.getElementById("chat_in_support");
+  chat_in_support.style.display = "block";
+
+    // Pegar os valores nos campos de email e texto
+  const email = document.getElementById("email").value;
+  const text = document.getElementById("txt_help").value;
+
+  socket.on('connect', () =>{
+    const params = {email, text};
+    socket.emit('client_first_access', params, (call, err) =>{
+      if(err) {
+        console.log(err);
+      }else {
+        console.log(call);
+      }
+    });
+  });
+});
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,6 @@
-import express from 'express';
-import "./database"
-import { routes } from './routes'
+import { http } from './http';
+import './websocket/client';
 
-const app = express();
-
-app.use(express.json());
-app.use(routes);
-
-
-app.get('/', (req, res) => {
-    return res.send('Hello World!');
-});
-
-
-app.post('/users', (req, res) => {
-    return res.send('Rota POST');
-})
-
-
-app.listen(3333, () => {
+http.listen(3333, () => {
     console.log('Listening on port 3333')
 });
